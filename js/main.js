f415bb8f812df623f4f28fd03db2522962ebf3ab
YouYou-happy <18251825510@163.com>
2017-05-09 21:20:46 +0800

    html&background
    搭建html网页结构
    绘制背景

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var can1;
+var can2;
+
+var ctx1;
+var ctx2;     //对应的场景
+
+var canWidth;
+var canHeight;
+
+var lastTime; //上一帧执行的时间
+var deltaTime;//两帧之间的时间差
+
+var bgPic = new Image();
+document.body.onload = game;
+function game() {
+	init();
+	lastTime = Date.now();//初始化
+	deltaTime = 0;
+	gameloop();
+}
+
+function init()
+{
+	//获得canvas context
+	can1 = document.getElementById("canvas1");//fishes, dust, UI, circle
+	ctx1 = can1.getContext('2d');
+	can2 = document.getElementById("canvas2");//background, ane, fruits
+	ctx2 = can2.getContext('2d');    //画布上的画笔来画画
+
+	bgPic.src = "./src/background.jpg";
+
+	canWidth = can1.width;
+	canHeight = can1.height;
+}
+function gameloop()
+{
+	window.requestAnimFrame(gameloop);//setInterval,setTimeout,frame per second
+	var now = Date.now();
+	deltaTime = now - lastTime;
+	lastTime = now;//更新上次的时间
+
+	drawBackground();
+}
