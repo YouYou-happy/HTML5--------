8b923feeb42b2c8493f3f0e57a187e1af8201fe1
YouYou-happy <18251825510@163.com>
2017-05-16 19:15:41 +0800

    swing animation of anemones
    海葵摆动动画

--- js/ane.js
+++ js/ane.js
@@ -1,20 +1,31 @@
 //定义一个海葵的类
 var aneObj = function()    
 {
-	this.x = [];
-	this.len = [];
+	//start point, control point, end point(sin)
+	this.rootx = [];
+	this.headx = [];
+	this.heady = [];
+	this.amp = [];
+	this.alpha = 0;
+	//this.len = [];
 }
 aneObj.prototype.num = 50;
 aneObj.prototype.init = function()
 {
+	//var h = can1.height;
 	for (var i = 0; i < this.num; i++)
 	{
-		this.x[i] = i * 20 + Math.random() * 20;//[0, 1),20是间距
-		this.len[i] = 200 + Math.random() * 50;//海葵高度
+		this.rootx[i] = i * 16 + Math.random() * 20;//[0, 1),20是间距
+		this.headx[i] = this.rootx[i];
+		this.heady[i] = canHeight - 250 + Math.random() * 50;
+		this.amp[i] = Math.random() * 50 + 50;
+		//this.len[i] = 200 + Math.random() * 50;//海葵高度
 	}
 }
 aneObj.prototype.draw = function()
 {
+	this.alpha += deltaTime * 0.0008; //摆动速度
+	var l = Math.sin(this.alpha);//[-1, 1]
 	ctx1.save();
 	ctx1.globalAlpha = 0.6;
 	ctx1.lineWidth = 20;
@@ -24,8 +35,9 @@ aneObj.prototype.draw = function()
 	{
 		//beginPath, moveTo, lineTo, stroke, strokeStyle, lineWidth, lineCap, globalAlpha
 		ctx1.beginPath();
-		ctx1.moveTo(this.x[i], canHeight);
-		ctx1.lineTo(this.x[i], canHeight - this.len[i]);
+		ctx1.moveTo(this.rootx[i], canHeight);
+		ctx1.quadraticCurveTo(this.rootx[i], canHeight - 100, this.headx[i] + l * this.amp[i], this.heady[i]);
+		//ctx1.lineTo(this.x[i], canHeight - this.len[i]);
 		ctx1.stroke();
 	}
 	ctx1.restore();
